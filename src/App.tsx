12a6aa0fbe7c3f166ae56027b603698a18757c4f
yKicchan <ykicchanapp@gmail.com>
2020-05-30 15:51:07 +0900

    add: ついでにrecoil入れてみた

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import List from './List/List'
-import './App.scss'
+import React from "react"
+import { RecoilRoot } from "recoil"
+
+import List from "./List/List"
+
+import "./App.scss"
 
 const App = () => {
   return (
-    <>
+    <RecoilRoot>
       <div></div>
       <List length={10} />
-    </>
+    </RecoilRoot>
   )
 }
 
-export default App;
+export default App
